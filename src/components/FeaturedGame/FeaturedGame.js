44250ba719b34a715982414d9b443599a4a3951b
Afuza <thegannoz@gmail.com>
2020-04-21 15:18:23 +0700

    Changed FeaturedGame styling
    Changes styling to match TrendingGame

--- src/components/FeaturedGame/FeaturedGame.js
+++ src/components/FeaturedGame/FeaturedGame.js
@@ -5,6 +5,11 @@ import styles from "./FeaturedGame.module.css";
 const FeaturedGame = (props) => {
   return (
     <div className={styles.featuredGame}>
+      <img
+        src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/hpisfx4wmws8ov1n9d1j.jpg"
+        alt="screenshot"
+        className={styles.screenshot}
+      />
       <div className={styles.container}>
         <img
           src="https://images.igdb.com/igdb/image/upload/t_cover_big/co1trn.jpg"
