02f2dacb2dfc8dd0a887b3540b9a979cef19d21c
Afuza <thegannoz@gmail.com>
2020-04-21 15:09:01 +0700

    Added TrendingGame

---
+++ src/components/TrendingGame/TrendingGame.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import styles from "./TrendingGame.module.css";
+
+const TrendingGame = (props) => {
+  return (
+    <div className={styles.trendingGame}>
+      <img
+        src="https://images.igdb.com/igdb/image/upload/t_screenshot_med/ozg4wanokhst4uehgkxs.jpg"
+        alt="screenshot"
+        className={styles.screenshot}
+      />
+      <div className={styles.container}>
+        <img
+          src="https://images.igdb.com/igdb/image/upload/t_cover_big/zvicevewoj0hfcqjaqx6.jpg"
+          alt="Cover"
+          className={styles.cover}
+        />
+        <div className={styles.infoContainer}>
+          <h1 className={styles.name}>{props.name}</h1>
+          <h3 className={styles.genres}>{props.genres.join(", ")}</h3>
+          <p className={styles.storyline}>{props.storyline}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrendingGame;
--- src/container/Main/Main.js
+++ src/container/Main/Main.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import FeaturedGame from "../../components/FeaturedGame/FeaturedGame";
 import GameCoverDisplay from "../../components/GameCoverDisplay/GameCoverDisplay";
+import TrendingGame from "../../components/TrendingGame/TrendingGame";
 import GameScreenshotDisplay from "../../components/GameScreenshotDisplay/GameScreenshotDisplay";
 
 import styles from "./Main.module.css";
@@ -18,10 +19,8 @@ class Main extends Component {
           <FeaturedGame
             name="Spiral Knights"
             genres={["RPG", "Adventure"]}
-            storyline="You have crashed. You are stranded. But you are not alone. The Spiral 
-            Knights have awoken on an alien world. Their equipment stores have been 
-            raided and their starship, The Skylark, will not recover from the crash. Now 
-            they must work together to survive on a journey that will take them to the 
+            storyline="You have crashed. You are stranded. But you are not alone. The Spiral Knights have awoken on an alien world. Their equipment stores have been 
+            raided and their starship, The Skylark, will not recover from the crash. Now they must work together to survive on a journey that will take them to the 
             very core of the world."
           />
           <SectionTitle title="latest releases" />
@@ -31,6 +30,11 @@ class Main extends Component {
             rating="89"
           />
           <SectionTitle title="trending" />
+          <TrendingGame
+            name="Mount & Blade: Bannerlord"
+            genres={["RPG", "Action", "Simulator"]}
+            storyline="Mount & Blade II: Bannerlord is the eagerly awaited sequel to the acclaimed medieval combat simulator and role-playing game Mount & Blade: Warband. Set 200 years before, it expands both the detailed fighting system and the world of Calradia. Bombard mountain fastnesses with siege engines, establish secret criminal empires in the back alleys of cities, or charge into the thick of chaotic battles in your quest for power."
+          />
           <GameScreenshotDisplay
             name="Skyrim"
             genre={["Action", "Fantasy"][0]}
