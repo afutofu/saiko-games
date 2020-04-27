779a9e79f97df1988f8c7c787f23c9ec05cebb20
Afuza <thegannoz@gmail.com>
2020-04-27 10:09:39 +0700

    Added placeholder content to GameCoverDisplay2

---
+++ src/components/GameCoverDisplay2/GameCoverDisplay2.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import styles from "./GameCoverDisplay2.module.css";
+
+const GameCoverDisplay2 = (props) => {
+  return (
+    <div className={styles.gameCoverDisplay}>
+      <img
+        src="https://images.igdb.com/igdb/image/upload/t_cover_big/co1x7j.jpg"
+        className={styles.cover}
+        alt="cover"
+      />
+      <div className={styles.infoContainer}>
+        <h3 className={styles.name}>{props.name}</h3>
+        <p className={styles.genres}>{props.genres.join(" ")}</p>
+        <p className={styles.storyline}>{props.storyline}</p>
+        <h3 className={styles.rating}>{props.rating}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default GameCoverDisplay2;
--- src/container/GameSearchPage/GameSearchPage.js
+++ src/container/GameSearchPage/GameSearchPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import GameCoverDisplay2 from "../../components/GameCoverDisplay2/GameCoverDisplay2";
 
 import styles from "./GameSearchPage.module.css";
 
@@ -17,6 +18,15 @@ class GameSearchPage extends Component {
         <div className={styles.background} />
         <div className={styles.container}>
           <SectionTitle title="games" />
+          <GameCoverDisplay2
+            onGameClick={this.onGameClick}
+            name="Spiral Knights"
+            genres={["RPG", "Adventure"]}
+            storyline="You have crashed. You are stranded. But you are not alone. The Spiral Knights have awoken on an alien world. Their equipment stores have been 
+                raided and their starship, The Skylark, will not recover from the crash. Now they must work together to survive on a journey that will take them to the 
+                very core of the world."
+            rating={81}
+          />
         </div>
       </div>
     );
