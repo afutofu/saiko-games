cd78a97ac960b76cfc8a91679286fd564a68a984
Afuza <theafuza@gmail.com>
2020-10-18 21:34:53 +0700

    added code for heroku deployment

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ app.post("/api/games", (req, res) => {
     });
 });
 
+// SERVE STATIC ASSETS IF IN PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (_req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
   console.log("Server now listening on port 5000");
 });
